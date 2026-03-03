0f4fdc3364dbd56ea888551577bd8f2802f1e435
candelario02 <huamancarrioncande22@gmail.com>
2026-03-03 13:28:38 -0500

    Corrigiendo import App_Sistema

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css' // <-- CAMBIAMOS index.css POR App.css
-import App from './App_Sistema';
+import App from './App_Sistema.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
