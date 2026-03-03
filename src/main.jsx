ab0c3569dba169a7b18a69d3cf03db32cfbdd810
candelario02 <huamancarrioncande22@gmail.com>
2026-03-03 13:19:03 -0500

    Cambio de nombre a App_Sistema para Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css' // <-- CAMBIAMOS index.css POR App.css
-import App from './App';
+import App from './App_Sistema';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
