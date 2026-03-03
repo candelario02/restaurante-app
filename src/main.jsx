e20c6133c7fbba615fb25a05c37f805476fcc28a
candelario02 <huamancarrioncande22@gmail.com>
2026-03-03 12:54:20 -0500

    FORZANDO mayusculas y dependencias PDF

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css' // <-- CAMBIAMOS index.css POR App.css
-import App from './App.jsx'
-
+import App from './App'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
