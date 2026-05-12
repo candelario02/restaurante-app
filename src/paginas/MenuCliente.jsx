ceb5104f6733d2ab00b1893070b2a361733720ea
candelario02 <huamancarrioncande24@gmail.com>
2026-05-11 20:30:30 -0500

    usando el useMemo

--- src/paginas/MenuCliente.jsx
+++ src/paginas/MenuCliente.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { db } from "../firebase/config";
 import { doc, onSnapshot } from "firebase/firestore";
 import Swal from "sweetalert2";
