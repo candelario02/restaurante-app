0854d3805c8227f59900b8500f94d4eb3eb1f9b1
candelario02 <huamancarrioncande24@gmail.com>
2026-05-29 13:23:26 -0500

    use meno

--- src/paginas/TvMenuBoard.jsx
+++ src/paginas/TvMenuBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import { doc, onSnapshot, collection, query, where } from "firebase/firestore";
 import { db } from "../firebase/config";
 import "../estilos/tvMenuBoard.css";
